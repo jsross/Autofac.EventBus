610b706f2c78393b7c54c16403707197ca5ba9f7
jsross <james.stanton.ross@gmail.com>
2017-05-08 23:14:56 -0400

    Adding missing files

---
+++ Samples/Sample1/Config/AutofacConfig.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Core.EventManagement.Configuration;
+using Sample1.Business.Abstract;
+using Sample1.Business.Concrete;
+using System.Reflection;
+
+namespace Sample1.Config
+{
+    public class AutofacConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Manager>()
+                   .As<IManager>()
+                   .InstancePerLifetimeScope()
+                   .EnableInterfaceInterceptors();
+
+            builder.RegisterType<EventHandler>()
+                   .InstancePerLifetimeScope();
+
+            builder.RegisterModule(new WorkflowModule(Assembly.Load("Sample1")));
+
+            var container = builder.Build();
+
+            return container;
+        }
+    }
+}
