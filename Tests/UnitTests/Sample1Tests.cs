b6c836f8052954d11f8c1927e209173d61409e06
jsross <james.stanton.ross@gmail.com>
2017-05-09 20:19:38 -0400

    Adding Sample1Tests

---
+++ Tests/UnitTests/Sample1Tests.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using NUnit.Framework;
+using Sample1.Business.Abstract;
+using Sample1.Config;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Sample1Tests
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var container = AutofacConfig.Configure();
+
+            ILifetimeScope scope = container.BeginLifetimeScope();
+
+            var manager = scope.Resolve<IManager>();
+
+            manager.DoSomething();
+        }
+    }
+}
